7ff3d289dca7fb3aa0c2e998c3acdae20d975ecc
Bert Vermeulen <bert@biot.com>
2024-05-26 23:05:18 +0200

    build: Move pico-specific stuff out of project.cmake

--- project.cmake
+++ project.cmake
@@ -7,25 +7,7 @@ set(PROJ_VER "0.0")
 # CLI INTERFACE - 0: use UART for CLI (default), 1: use USB for CLI
 set(CLI_IFACE 0)
 
+set(PLATFORM rp2040)
+
 # BOARD - set the board being used (i.e. 'pico', 'pico_w', etc)
 set(BOARD pico)
-
-# ------------- Don't change anything below this line ------------
-
-# BOARD
-set(PICO_BOARD ${BOARD})
-
-# Raspberry Pi Pico (RP2040) SDK
-include(pico_sdk_import.cmake)
-
-if(PICO_BOARD STREQUAL "pico_w")
-    add_definitions(-DUSING_PICOW) # Pass this parameter to the preprocessor if board type is pico_w since some HW config needs to change
-endif()
-
-# initialize the Pico/RP2040 SDK
-pico_sdk_init()
-
-set(hardware_libs "pico_unique_id" "pico_stdlib")
-set(hardware_includes hardware/rp2040)
-set(hardware_subdir ${PROJECT_SOURCE_DIR}/hardware/rp2040)
-
