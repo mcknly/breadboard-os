e3573ae644bbf502ac12c4119d447e27d26b796b
Alec Lanter <kintar1900@gmail.com>
2024-06-20 18:23:08 -0500

    feat: update hardware/rp2040/prebuild.cmake to add cyw43 libs when using pico_w

--- hardware/rp2040/prebuild.cmake
+++ hardware/rp2040/prebuild.cmake
@@ -5,3 +5,7 @@ include(${hardware_dir}/pico_sdk_import.cmake)
 
 set(hardware_includes ${hardware_dir})
 set(hardware_libs "pico_unique_id" "pico_stdlib")
+
+if(PICO_BOARD STREQUAL "pico_w")
+    list(APPEND hardware_libs "pico_cyw43_arch_none")
+endif()
