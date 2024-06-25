3f0a0db23fc4350cc74d3e8607ac9bea30ce5dde
Alec Lanter <kintar1900@gmail.com>
2024-06-25 16:53:04 -0500

    feat(#20): need configUSE_PASSIVE_IDLE_HOOK defined for SMP versions of FreeRTOS

--- rtos/FreeRTOSConfig.h
+++ rtos/FreeRTOSConfig.h
@@ -55,6 +55,7 @@
 #define configUSE_PREEMPTION                    1
 #define configUSE_TICKLESS_IDLE                 0
 #define configUSE_IDLE_HOOK                     0
+#define configUSE_PASSIVE_IDLE_HOOK             0
 #define configUSE_TICK_HOOK                     0
 #define configTICK_RATE_HZ                      ( ( TickType_t ) 1000 )
 #define configMAX_PRIORITIES                    32
@@ -110,7 +111,7 @@
 */
 
 /* SMP port only */
-#define configNUM_CORES                         2
+#define configNUMBER_OF_CORES                   2
 #define configTICK_CORE                         1
 #define configRUN_MULTIPLE_PRIORITIES           1
 
