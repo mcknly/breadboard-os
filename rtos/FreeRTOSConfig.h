7323fbfa51c828c3746832a7f0d6043841805067
Alec Lanter <kintar1900@gmail.com>
2024-06-27 16:55:06 -0500

    feat(#20): more necessary FreeRTOS config updates

--- rtos/FreeRTOSConfig.h
+++ rtos/FreeRTOSConfig.h
@@ -73,7 +73,7 @@
 #define configUSE_QUEUE_SETS                    1
 #define configUSE_TIME_SLICING                  1
 #define configUSE_NEWLIB_REENTRANT              0
-#define configENABLE_BACKWARD_COMPATIBILITY     0
+#define configENABLE_BACKWARD_COMPATIBILITY     1
 #define configNUM_THREAD_LOCAL_STORAGE_POINTERS 5
 
 /* System */
