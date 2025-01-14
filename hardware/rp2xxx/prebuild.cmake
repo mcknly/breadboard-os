e109d2b8a600faf65514bc0e5fb50fcb38dd5f43
Cavin McKinley <30472644+mcknly@users.noreply.github.com>
2025-01-13 21:57:47 -0600

    33 pico 2 w support (#34)
    * Change FreeRTOS port path for RP2350
    
    * Add pico2_w checks to prebuild.cmake for proper configuration
    
    * Add check in prebuild.cmake for unknown board type and error message

--- hardware/rp2xxx/prebuild.cmake
+++ hardware/rp2xxx/prebuild.cmake
@@ -19,16 +19,19 @@ set(hardware_libs   "pico_unique_id"
 )
 
 # FreeRTOS port subdirectory for this platform (relative to $FREERTOS_KERNEL_PATH)
-# If it is a RP2350 board the board name needs to be added here to select the right port
-if(PICO_BOARD STREQUAL "pico2")
-    set(freertos_port_path "portable/ThirdParty/GCC/RP2350_ARM_NTZ/FreeRTOS_Kernel_import.cmake")
-else()
+# all of the "boards" from the link above should be handled here to categorize
+# them as RP2040 or RP2350 type
+if(PICO_BOARD STREQUAL "pico" OR PICO_BOARD STREQUAL "pico_w")
     set(freertos_port_path "portable/ThirdParty/GCC/RP2040/FreeRTOS_Kernel_import.cmake")
+elseif(PICO_BOARD STREQUAL "pico2" OR PICO_BOARD STREQUAL "pico2_w")
+    set(freertos_port_path "portable/ThirdParty/Community-Supported-Ports/GCC/RP2350_ARM_NTZ/FreeRTOS_Kernel_import.cmake")
+else()
+    message(FATAL_ERROR "Board type unknown to BBOS: ${PICO_BOARD} - see hardware/rp2xxx/prebuild.cmake for instructions")
 endif()
 
 # If using a Pico wireless variant, include the cyw43 library
 # Any other board names that use cyw43 would have to be added here too
-if(PICO_BOARD STREQUAL "pico_w")
+if(PICO_BOARD STREQUAL "pico_w" OR PICO_BOARD STREQUAL "pico2_w")
     list(APPEND hardware_libs pico_cyw43_arch_none)
     add_compile_definitions(USING_CYW43)
 endif()
